dca1586f6f594d729c79f83d239cea7282c17fb4
ken <kennedywanjahi@gmail.com>
2019-05-19 21:19:03 +0300

    file to make payout period dynamic

---
+++ addpayoutperiod.php
@@ -0,0 +1,115 @@
+<?php
+include ('header.php');
+ ?>
+ <?php
+if (isset($_POST['addperiod'])){
+  addperiod();
+}
+include 'sidebar.php';
+  ?>
+            <!-- login Start-->
+            <div class="login-form-area mg-t-30 mg-b-40">
+                <div class="container-fluid">
+                    <div class="row">
+                        <div class="col-lg-4"></div>
+                        <form id="adminpro-form" class="adminpro-form" method="post">
+                            <div class="col-lg-4">
+                                <div class="login-bg">
+                                    <div class="row">
+                                        <div class="col-lg-12">
+                                            <div class="login-title">
+                                                <h1>Add Payout Period</h1>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="row">
+                                        <div class="col-lg-4">
+                                            <div class="login-input-head">
+                                                <p>Year</p>
+                                            </div>
+                                        </div>
+                                        <div class="col-lg-8">
+                                            <div class="login-input-area">
+                                                <input type="text" name="year" placeholder=""/>
+                                                <i class="fa fa-calendar login-user" aria-hidden="true"></i>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="row">
+                                        <div class="col-lg-4">
+                                            <div class="login-input-head">
+                                                <p>Period</p>
+                                            </div>
+                                        </div>
+                                        <div class="col-lg-8">
+                                            <div class="login-input-area">
+                                                <input type="text" name="period" />
+                                                <i class="fa fa-user login-user" aria-hidden="true"></i>
+                                            </div>
+                                        </div>
+                                    </div>
+
+
+                                    <div class="row">
+                                        <div class="col-lg-4">
+
+                                        </div>
+                                        <div class="col-lg-8">
+                                            <div class="login-button-pro">
+                                                <input type="submit" value="Add Payout Period" name="addperiod" class="login-button login-button-lg"></input>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </form>
+                        <div class="col-lg-4"></div>
+                    </div>
+                </div>
+            </div>
+            <!-- login End-->
+        </div>
+    </div>
+
+    <!-- Footer Start-->
+    <div class="footer-copyright-area">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="footer-copy-right">
+                        <p>Copyright &#169;</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- Footer End-->
+    <!-- jquery
+		============================================ -->
+    <script src="js/vendor/jquery-1.11.3.min.js"></script>
+    <!-- bootstrap JS
+		============================================ -->
+    <script src="js/bootstrap.min.js"></script>
+    <!-- meanmenu JS
+		============================================ -->
+    <script src="js/jquery.meanmenu.js"></script>
+    <!-- mCustomScrollbar JS
+		============================================ -->
+    <script src="js/jquery.mCustomScrollbar.concat.min.js"></script>
+    <!-- sticky JS
+		============================================ -->
+    <script src="js/jquery.sticky.js"></script>
+    <!-- scrollUp JS
+		============================================ -->
+    <script src="js/jquery.scrollUp.min.js"></script>
+    <!-- form validate JS
+		============================================ -->
+    <script src="js/jquery.form.min.js"></script>
+    <script src="js/jquery.validate.min.js"></script>
+    <script src="js/form-active.js"></script>
+    <!-- main JS
+		============================================ -->
+    <script src="js/main.js"></script>
+</body>
+
+</html>
