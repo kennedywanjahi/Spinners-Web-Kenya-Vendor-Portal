eb5591f2a1cb4eb3b2f7a2838df1887fc66e144e
root <kennedywanjahi@gmail.com>
2019-06-16 16:30:46 +0300

    sorting out weekly sales

---
+++ exportweeklydetails.php
@@ -0,0 +1,69 @@
+<?php
+ //export.php
+ $period_id = $_POST["period"];
+ if(!empty($_FILES["excel_file"]))
+ {
+      $connect = mysqli_connect("localhost", "root", "", "spinners");
+      $file_array = explode(".", $_FILES["excel_file"]["name"]);
+      if($file_array[1] == "xlsx")
+      {
+           include("PHPExcel/IOFactory.php");
+           $output = '';
+           $output .= "
+           <label class='text-success'>Data Inserted</label>
+                <table class='table table-bordered'>
+                     <tr>
+                          <th>Period</th>
+                          <th>VendorCode</th>
+                          <th>Item No</th>
+                          <th>Item Name</th>
+                          <th>Alternate Look Up</th>
+                          <th>Attribute</th>
+                          <th>Size</th>
+                          <th>Qty Sold</th>
+                          <th>Ext Cost</th>
+                     </tr>
+                     ";
+           $object = PHPExcel_IOFactory::load($_FILES["excel_file"]["tmp_name"]);
+           foreach($object->getWorksheetIterator() as $worksheet)
+           {
+                $highestRow = $worksheet->getHighestRow();
+                for($row=2; $row<=$highestRow; $row++)
+                {
+
+                     $vendorCode = mysqli_real_escape_string($connect, $worksheet->getCellByColumnAndRow(0, $row)->getValue());
+                     $itemNo = mysqli_real_escape_string($connect, $worksheet->getCellByColumnAndRow(1, $row)->getValue());
+                     $itemName = mysqli_real_escape_string($connect, $worksheet->getCellByColumnAndRow(2, $row)->getValue());
+                     $alu = mysqli_real_escape_string($connect, $worksheet->getCellByColumnAndRow(3, $row)->getValue());
+                     $attribute = mysqli_real_escape_string($connect, $worksheet->getCellByColumnAndRow(4, $row)->getValue());
+                     $size = mysqli_real_escape_string($connect, $worksheet->getCellByColumnAndRow(5, $row)->getValue());
+                     $qtySold = mysqli_real_escape_string($connect, $worksheet->getCellByColumnAndRow(6, $row)->getValue());
+                     $extCost = mysqli_real_escape_string($connect, $worksheet->getCellByColumnAndRow(7, $row)->getValue());
+                     $query = "
+                     INSERT INTO weekly_details
+                     (PeriodId, Vendor, ItemNo, ItemName, Alu, Attribute, Size, QtySold, ExtCost)
+                     VALUES ('".$period_id."', '".$vendorCode."', '".$itemNo."', '".$itemName."', '".$alu."', '".$attribute."', '".$size."', '".$qtySold."', '".$extCost."')";
+                     mysqli_query($connect, $query);
+                     $output .= '
+                     <tr>
+                          <td>'.$vendorCode.'</td>
+                          <td>'.$itemNo.'</td>
+                          <td>'.$itemName.'</td>
+                          <td>'.$alu.'</td>
+                          <td>'.$attribute.'</td>
+                          <td>'.$size.'</td>
+                          <td>'.$qtySold.'</td>
+                          <td>'.$extCost.'</td>
+                     </tr>
+                     ';
+                }
+           }
+           $output .= '</table>';
+           echo $output;
+      }
+      else
+      {
+           echo '<label class="text-danger">Invalid File</label>';
+      }
+ }
+ ?>
---
+++ exportweeklysummary.php
@@ -0,0 +1,56 @@
+<?php
+ //export.php
+ $period_id = $_POST["period"];
+
+ if(!empty($_FILES["excel_file"]))
+ {
+      $connect = mysqli_connect("localhost", "root", "", "spinners");
+      $file_array = explode(".", $_FILES["excel_file"]["name"]);
+      if($file_array[1] == "xlsx")
+      {
+           include("PHPExcel/IOFactory.php");
+           $output = '';
+           $output .= "
+           <label class='text-success'>Data Inserted</label>
+                <table class='table table-bordered'>
+                     <tr>
+
+                          <th>VendorCode</th>
+
+                          <th>Vendor Name</th>
+                          <th>Amount</th>
+                     </tr>
+                     ";
+           $object = PHPExcel_IOFactory::load($_FILES["excel_file"]["tmp_name"]);
+           foreach($object->getWorksheetIterator() as $worksheet)
+           {
+                $highestRow = $worksheet->getHighestRow();
+                for($row=2; $row<=$highestRow; $row++)
+                {
+                     $vendorCode = mysqli_real_escape_string($connect, $worksheet->getCellByColumnAndRow(0, $row)->getValue());
+                     $vendorName = mysqli_real_escape_string($connect, $worksheet->getCellByColumnAndRow(1, $row)->getValue());
+                     $amount= mysqli_real_escape_string($connect, $worksheet->getCellByColumnAndRow(2, $row)->getValue());
+                     $query = "
+                     INSERT INTO weekly_totals
+                     (PeriodId,  VendorCode, VendorName, Total)
+                     VALUES ('".$period_id."', '".$vendorCode."', '".$vendorName."', '".$amount."')";
+                     mysqli_query($connect, $query);
+                     $output .= '
+                     <tr>
+                          <td>'.$vendorCode.'</td>
+                          <td>'.$vendorName.'</td>
+                          <td>'.$amount.'</td>
+
+                     </tr>
+                     ';
+                }
+           }
+           $output .= '</table>';
+           echo $output;
+      }
+      else
+      {
+           echo '<label class="text-danger">Invalid File</label>';
+      }
+ }
+ ?>
