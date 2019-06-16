299f7f2c249a9183a6d7f487c93f17eec422eca5
root <kennedywanjahi@gmail.com>
2019-06-16 15:25:11 +0300

    making the vendor update form downloadable

---
+++ downloads.php
@@ -0,0 +1,121 @@
+<?php include 'header.php'; ?>
+    <!-- Header top area start-->
+    <div class="wrapper-pro">
+        <?php include 'sidebar.php'; ?>
+            <!-- Breadcome start-->
+            <div class="breadcome-area mg-b-30 small-dn">
+                <div class="container-fluid">
+                    <div class="row">
+                        <div class="col-lg-12">
+                            <div class="breadcome-list shadow-reset">
+                                <div class="row">
+                                    <div style="text-align:center" class="col-lg-12">
+                                        <ul>
+
+                                        <li><span class="bread-blod"><h1>Downloads</h1></span>
+                                            </li>
+                                        </ul>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="breadcome-area mg-b-30 small-dn">
+                <div class="container-fluid">
+                    <div class="row">
+                        <div class="col-lg-12">
+                            <div class="breadcome-list shadow-reset">
+                                <div class="row">
+                                    <div style="text-align:center" >
+                                        <ul>
+
+                                            <li><span class="bread-blod"><h3>Vendor Update Form</h3> </span></li><br>
+                                            <li><span class="bread-blod">Click download, fill it out and email it to us.</span></li><br>
+                                            <li style="text-align:centre"><span class="bread-blod"></span>  <a href="docs/form.pdf">Click here to download <i class="fas fa-download"></i></a> </li>
+                                        </ul>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- Breadcome End-->
+            <div class="user-profile-area">
+                <div class="container-fluid">
+                    <div class="row">
+                        <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- jquery
+		============================================ -->
+    <script src="js/vendor/jquery-1.11.3.min.js"></script>
+    <!-- bootstrap JS
+		============================================ -->
+    <script src="js/bootstrap.min.js"></script>
+    <!-- meanmenu JS
+		============================================ -->
+    <script src="js/jquery.meanmenu.js"></script>
+    <!-- mCustomScrollbar JS
+		============================================ -->
+    <script src="js/jquery.mCustomScrollbar.concat.min.js"></script>
+    <!-- sticky JS
+		============================================ -->
+    <script src="js/jquery.sticky.js"></script>
+    <!-- scrollUp JS
+		============================================ -->
+    <script src="js/jquery.scrollUp.min.js"></script>
+    <!-- counterup JS
+		============================================ -->
+    <script src="js/counterup/jquery.counterup.min.js"></script>
+    <script src="js/counterup/waypoints.min.js"></script>
+    <!-- rounded-counter JS
+		============================================ -->
+    <script src="js/rounded-counter/jquery.countdown.min.js"></script>
+    <script src="js/rounded-counter/jquery.knob.js"></script>
+    <script src="js/rounded-counter/jquery.appear.js"></script>
+    <script src="js/rounded-counter/knob-active.js"></script>
+    <!-- peity JS
+		============================================ -->
+    <script src="js/peity/jquery.peity.min.js"></script>
+    <script src="js/peity/peity-active.js"></script>
+    <!-- sparkline JS
+		============================================ -->
+    <script src="js/sparkline/jquery.sparkline.min.js"></script>
+    <script src="js/sparkline/sparkline-active.js"></script>
+    <!-- flot JS
+		============================================ -->
+    <script src="js/flot/jquery.flot.js"></script>
+    <script src="js/flot/jquery.flot.tooltip.min.js"></script>
+    <script src="js/flot/jquery.flot.resize.js"></script>
+    <script src="js/flot/widget-flot-chart-active.js"></script>
+    <!-- map JS
+		============================================ -->
+    <script src="js/map/raphael.min.js"></script>
+    <script src="js/map/jquery.mapael.js"></script>
+    <script src="js/map/france_departments.js"></script>
+    <script src="js/map/world_countries.js"></script>
+    <script src="js/map/usa_states.js"></script>
+    <script src="js/map/map-active.js"></script>
+    <!--  editable JS
+		============================================ -->
+    <script src="js/jquery.mockjax.js"></script>
+    <script src="js/mock-active.js"></script>
+    <script src="js/select2.js"></script>
+    <script src="js/moment.min.js"></script>
+    <script src="js/bootstrap-datetimepicker.js"></script>
+    <script src="js/bootstrap-editable.js"></script>
+    <script src="js/xediable-active.js"></script>
+    <!-- main JS
+		============================================ -->
+    <script src="js/main.js"></script>
+</body>
+
+</html>
