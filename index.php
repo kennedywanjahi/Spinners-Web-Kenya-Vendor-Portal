d5ee6c3d644c4c1512a468390361eab1cc275c4f
root <kennedywanjahi@gmail.com>
2019-07-09 17:09:15 +0300

    front end tweaks

--- index.php
+++ index.php
@@ -61,7 +61,7 @@ if (isset($_POST['login'])){
                                             <div class="row">
                                                 <div class="col-lg-12">
                                                     <div class="forgot-password">
-                                                        <a href="www.spinnerswebkenya.com">Not a Vendor ? Go back Home</a>
+                                                        <a href="http://www.spinnerswebkenya.com">Not a Vendor ? Go back Home</a>
                                                     </div>
                                                 </div>
                                             </div>
