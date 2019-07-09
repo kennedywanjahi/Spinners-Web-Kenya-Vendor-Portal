d9f767b1263faaa64dbc617f95b363d7d6341d05
root <kennedywanjahi@gmail.com>
2019-07-09 12:49:01 +0300

    changing database variablies

--- includes/db.php
+++ includes/db.php
@@ -1,19 +1,21 @@
 <?php
-$db['db_host']="localhost";
-$db['db_user']="root";
-$db['db_pass']="";
-$db['db_name']="spinners";
+$db['db_host']="127.0.0.1";
+$db['db_user']="kenyawea_backoffice";
+$db['db_pass']="DB@SWKPortal!";
+$db['db_name']="kenyawea_spinners_portal";
 
 foreach ($db as $key => $value) {
   define(strtoupper($key), $value);
 
 }
 
-$connection = mysqli_connect(DB_HOST,DB_USER,'',DB_NAME);
-// if ($connection) {
-//   echo "we are connected";
-// }
-
+$connection = mysqli_connect(DB_HOST,DB_USER,DB_PASS,DB_NAME);
+if ($connection) {
+  echo "we are connected";
+}
+else{
+  die("Some error occurred during connection " . mysqli_error($connection));
+}
 
 
  ?>
--- sidebar.php
+++ sidebar.php
@@ -67,6 +67,10 @@ if (!isset($_SESSION['username'])) {
             </li>
             <li class="nav-item"><a href="weeklyperiods.php" ><i class="fas big-icon fa-calendar-week"></i> <span class="mini-dn">Weekly Periods</span> <span class="indicator-right-menu mini-dn"><i class="fa indicator-mn fa-angle-left"></i></span></a>
             </li>
+          </li>
+          <li class="nav-item"><a href="weeklyperiods.php" ><i class="fas big-icon fa-calendar-week"></i> <span class="mini-dn">Payment Methods</span> <span class="indicator-right-menu mini-dn"><i class="fa indicator-mn fa-angle-left"></i></span></a>
+          </li>
+        
                 <?php
               }
                ?>
