cdd5ee16c6c6673b8bbad1bba681d6a1120f1f63
root <kennedywanjahi@gmail.com>
2019-07-11 11:10:09 +0300

    front end tweaks

--- header.php
+++ header.php
@@ -57,7 +57,7 @@ include 'includes/functions.php';
 		============================================ -->
 
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@8"></script>
-    <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.4.2/jquery.min.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
 
 
 
