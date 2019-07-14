09da5d5afe30527a367906ae5f0fe45d6f8cb3e5
root <kennedywanjahi@gmail.com>
2019-07-14 12:20:34 +0300

    front end tweaks

--- vendor_home.php
+++ vendor_home.php
@@ -88,7 +88,7 @@ echo '<script>window.location="admin_home.php" </script>';
                                                                  vendorpayoutdetails.php?id={$period_id}
                                                                  ";
                                                                  ?>
-                                                                 ">View Details<i class="fas fa-arrow-circle-right"></i></i></span>
+                                                                 ">View Details <i class="fas fa-arrow-circle-right"></i></i></span>
                                                              </div>
                                                              <div class="clear"></div>
                                                          </div>
