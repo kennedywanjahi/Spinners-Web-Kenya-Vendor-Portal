668512b93e85a325db1b691619506bb308e0f43b
root <kennedywanjahi@gmail.com>
2019-07-16 19:01:37 +0300

    file to export payment excel sheet into database

---
+++ exportpayment.php
@@ -0,0 +1,56 @@
+<?php
+ //export.php
+ $period_id = $_POST["period"];
+
+ if(!empty($_FILES["excel_file"]))
+ {
+      $connect = mysqli_connect("127.0.0.1", "kenyawea_backoffice", "DB@SWKPortal!", "kenyawea_spinners_portal");
+      $file_array = explode(".", $_FILES["excel_file"]["name"]);
+      if($file_array[1] == "xlsx")
+      {
+           include("PHPExcel/IOFactory.php");
+           $output = '';
+           $output .= "
+           <label class='text-success'>Data Inserted</label>
+                <table class='table table-bordered'>
+                     <tr>
+
+                          <th>VendorCode</th>
+
+                          <th>Vendor Name</th>
+                          <th>Method</th>
+                     </tr>
+                     ";
+           $object = PHPExcel_IOFactory::load($_FILES["excel_file"]["tmp_name"]);
+           foreach($object->getWorksheetIterator() as $worksheet)
+           {
+                $highestRow = $worksheet->getHighestRow();
+                for($row=2; $row<=$highestRow; $row++)
+                {
+                     $vendorCode = mysqli_real_escape_string($connect, $worksheet->getCellByColumnAndRow(0, $row)->getValue());
+                     $vendorName = mysqli_real_escape_string($connect, $worksheet->getCellByColumnAndRow(1, $row)->getValue());
+                     $method= mysqli_real_escape_string($connect, $worksheet->getCellByColumnAndRow(2, $row)->getValue());
+                     $query = "
+                     INSERT INTO payment_methods
+                     (PeriodId,  VendorCode, VendorName, Method)
+                     VALUES ('".$period_id."', '".$vendorCode."', '".$vendorName."', '".$method."')";
+                     mysqli_query($connect, $query);
+                     $output .= '
+                     <tr>
+                          <td>'.$vendorCode.'</td>
+                          <td>'.$vendorName.'</td>
+                          <td>'.$method.'</td>
+
+                     </tr>
+                     ';
+                }
+           }
+           $output .= '</table>';
+           echo $output;
+      }
+      else
+      {
+           echo '<label class="text-danger">Invalid File</label>';
+      }
+ }
+ ?>
