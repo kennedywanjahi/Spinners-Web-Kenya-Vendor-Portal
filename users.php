28664a68fbe89e1e2c14b4ad79024a2a40949658
root <kennedywanjahi@gmail.com>
2019-07-28 16:03:39 +0300

    making user table searchable

--- users.php
+++ users.php
@@ -57,7 +57,7 @@ if (isset($_GET["successd"])) {
 
                                             </div>
                                             <div class="datatable-dashv1-list custom-datatable-overright">
-                                                <table id="table" data-toggle="table" data-pagination="true" data-search="true" data-show-columns="true" data-show-pagination-switch="true" data-show-refresh="true" data-key-events="true" data-show-toggle="true" data-resizable="false" data-cookie="true" data-cookie-id-table="saveId" data-show-export="true" data-click-to-select="true" data-toolbar="#toolbar1">
+                                              <table id="table" data-toggle="table" data-pagination="true" data-search="true" data-show-columns="true" data-show-pagination-switch="true" data-show-refresh="true" data-key-events="true" data-show-toggle="true" data-resizable="true" data-cookie="true" data-cookie-id-table="saveId" data-show-export="true" data-click-to-select="true" data-toolbar="#toolbar1">
                                                     <thead>
                                                         <tr>
                                                             <th data-field="user">User</th>
