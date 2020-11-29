182017c4a58133f53da6445aa43c41213e773bc1
wanjahi <kennedywanjahi@gmail.com>
2020-11-29 16:48:37 +0300

    payout detail fixing

--- export.php
+++ export.php
@@ -44,12 +44,9 @@
                      $extCost = mysqli_real_escape_string($connect, $worksheet->getCellByColumnAndRow(8, $row)->getValue());
                      $query = "
                      INSERT INTO payout_details
-                     ( PeriodId, Vendor, ItemNo, ItemName, Alu, Vpc, Attribute, Size, QtySold, ExtCost)
-                     VALUES ('".$period_id."', '".$vendorCode."', '".$itemNo."', '".$itemName."', '".$alu."', '".$vpc."', '".$attribute."', '".$size."', '".$qtySold."', '".$extCost."')";
-                     $payoutquery = mysqli_query($connect, $query);
-                     if(!$payoutquery){
-+                       die("QUERY FAILED" .mysqli_error($connection));
-+                     }
+                     ( PeriodId, Vendor, ItemNo, ItemName, Alu, Vpc, Attribute, Size, OnHand, Cost)
+                     VALUES ('".$period_id."', '".$vendorCode"', '".$itemNo."', '".$itemName."', '".$alu."', '".$vpc."', '".$attribute."', '".$size."', '".$onHand."', '".$Cost."')";
+                     mysqli_query($connect, $query);
                      $output .= '
                      <tr>
                           <td>'.$vendorCode.'</td>
